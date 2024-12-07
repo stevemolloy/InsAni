9f39c746f1e78d2e6bde149e61f999f54b080e57
Stephen Molloy <sdmolloy@gmail.com>
2024-12-07 23:01:50 +0100

    Improvements to appearance and handling of the button

--- src/plugin.c
+++ src/plugin.c
@@ -6,19 +6,25 @@
 #include "plugin.h"
 #include "insani_lib.h"
 
-#define BACKGROUND_COLOUR (Color){0x28, 0x28, 0x28, 0xFF}
-#define TEAM_A_COLOUR RED
-#define TEAM_B_COLOUR BLUE
-#define TEAM_C_COLOUR GREEN
+#define DARK_COLOUR (Color){17, 16, 37, 0xFF}
+#define LIGHT_COLOUR (Color){0xFF, 0xFF, 0xFF, 0xFF}
+#define INTERMED_COLOUR (Color){0xCC, 0xCC, 0xCC, 0xFF}
+#define BACKGROUND_COLOUR DARK_COLOUR
+#define CTRLPANEL_COLOUR INTERMED_COLOUR
+#define BUTTON_COLOUR (Color){0x55, 0x55, 0x55, 0xFF}
+#define BUTTON_TEXT_COLOUR LIGHT_COLOUR
+#define TEAM_A_COLOUR MAROON
+#define TEAM_B_COLOUR SKYBLUE
+#define TEAM_C_COLOUR DARKGREEN
 #define TEAM_D_COLOUR BLACK
-#define TEAM_E_COLOUR PURPLE
-#define RECT_HEIGHT 12
-#define RECT_WIDTH 90
+#define TEAM_E_COLOUR DARKPURPLE
+#define RECT_HEIGHT 10
+#define RECT_WIDTH 80
 #define NUM_OF_ACHROS 20
 #define WORKING_TIME 1.0
-#define MOVING_TIME 0.5
+#define MOVING_TIME 0.25
 #define PANELPADDING 10
-#define BTN_HEIGHT 40
+#define BTN_HEIGHT 35
 
 typedef enum {
   ANI_PAUSED,
@@ -53,34 +59,34 @@ void plug_frame_update(PlugState state) {
 
   switch (global_state) {
     case WORKING: {
-      if (ani_state == ANI_RUNNING) {
       elapsed_time += GetFrameTime();
-        if (elapsed_time > WORKING_TIME) {
-          elapsed_time = 0.0;
-          global_state = MOVING;
-        }
-        rectA_rot = achr2degrees(state.job_list_A[frame_number].where);
-        rectB_rot = achr2degrees(state.job_list_B[frame_number].where);
-        rectC_rot = achr2degrees(state.job_list_C[frame_number].where);
-        rectD_rot = achr2degrees(state.job_list_D[frame_number].where);
-        rectE_rot = achr2degrees(state.job_list_E[frame_number].where);
+      if (elapsed_time > WORKING_TIME) {
+        elapsed_time = 0.0;
+        if (ani_state == ANI_RUNNING) global_state = MOVING;
       }
+      rectA_rot = achr2degrees(state.job_list_A[frame_number].where);
+      rectB_rot = achr2degrees(state.job_list_B[frame_number].where);
+      rectC_rot = achr2degrees(state.job_list_C[frame_number].where);
+      rectD_rot = achr2degrees(state.job_list_D[frame_number].where);
+      rectE_rot = achr2degrees(state.job_list_E[frame_number].where);
       break;
     }
     case MOVING: {
-      elapsed_time += GetFrameTime();
-      if (elapsed_time > MOVING_TIME) {
-        elapsed_time = 0.0;
-        global_state = WORKING;
-        frame_number = (frame_number + 1) % state.num_working_days;
+        if (elapsed_time > MOVING_TIME) {
+          elapsed_time = 0.0;
+          global_state = WORKING;
+          frame_number = (frame_number + 1) % state.num_working_days;
+        }
+      if ((ani_state == ANI_RUNNING) || (elapsed_time < MOVING_TIME)) {
+        elapsed_time += GetFrameTime();
+        size_t next_frame = (frame_number + 1) % state.num_working_days;
+        float t = elapsed_time / MOVING_TIME;
+        rectA_rot = lerpf(achr2degrees(state.job_list_A[frame_number].where), achr2degrees(state.job_list_A[next_frame].where), t);
+        rectB_rot = lerpf(achr2degrees(state.job_list_B[frame_number].where), achr2degrees(state.job_list_B[next_frame].where), t);
+        rectC_rot = lerpf(achr2degrees(state.job_list_C[frame_number].where), achr2degrees(state.job_list_C[next_frame].where), t);
+        rectD_rot = lerpf(achr2degrees(state.job_list_D[frame_number].where), achr2degrees(state.job_list_D[next_frame].where), t);
+        rectE_rot = lerpf(achr2degrees(state.job_list_E[frame_number].where), achr2degrees(state.job_list_E[next_frame].where), t);
       }
-      size_t next_frame = (frame_number + 1) % state.num_working_days;
-      float t = elapsed_time / MOVING_TIME;
-      rectA_rot = lerpf(achr2degrees(state.job_list_A[frame_number].where), achr2degrees(state.job_list_A[next_frame].where), t);
-      rectB_rot = lerpf(achr2degrees(state.job_list_B[frame_number].where), achr2degrees(state.job_list_B[next_frame].where), t);
-      rectC_rot = lerpf(achr2degrees(state.job_list_C[frame_number].where), achr2degrees(state.job_list_C[next_frame].where), t);
-      rectD_rot = lerpf(achr2degrees(state.job_list_D[frame_number].where), achr2degrees(state.job_list_D[next_frame].where), t);
-      rectE_rot = lerpf(achr2degrees(state.job_list_E[frame_number].where), achr2degrees(state.job_list_E[next_frame].where), t);
       break;
     }
   }
@@ -102,14 +108,19 @@ void plug_frame_update(PlugState state) {
   Rectangle teamE_rect = {.x = rectE_loc.x, .y = rectE_loc.y, .height = RECT_HEIGHT, .width = RECT_WIDTH};
 
   Rectangle ctrl_panel = {.x=height, .y=PANELPADDING, .width=width-height-(2*PANELPADDING), .height=height-(2*PANELPADDING)};
-  Rectangle start_stop_btn_rect = {.x=ctrl_panel.x+PANELPADDING, .y=ctrl_panel.y+PANELPADDING, .width=ctrl_panel.width/2-2*PANELPADDING, .height=BTN_HEIGHT};
+  Rectangle pause_btn_rect = {
+    .x=ctrl_panel.x+ctrl_panel.width/2+PANELPADDING, 
+    .y=ctrl_panel.y+ctrl_panel.height-PANELPADDING-BTN_HEIGHT,
+    .width=ctrl_panel.width/2-2*PANELPADDING, 
+    .height=BTN_HEIGHT
+  };
   Vector2 text_pos = MeasureTextEx(state.font, button_text(ani_state), state.fontsize, 0);
-  text_pos.x = start_stop_btn_rect.x + start_stop_btn_rect.width/2 - text_pos.x/2;
-  text_pos.y = start_stop_btn_rect.y + start_stop_btn_rect.height/2 - text_pos.y/2;
+  text_pos.x = pause_btn_rect.x + pause_btn_rect.width/2 - text_pos.x/2;
+  text_pos.y = pause_btn_rect.y + pause_btn_rect.height/2 - text_pos.y/2;
 
-  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+  if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
     Vector2 click_loc = GetMousePosition();
-    if (CheckCollisionPointRec(click_loc, start_stop_btn_rect)) {
+    if (CheckCollisionPointRec(click_loc, pause_btn_rect)) {
       if (ani_state == ANI_RUNNING) ani_state = ANI_PAUSED;
       else if (ani_state == ANI_PAUSED) ani_state = ANI_RUNNING;
     }
@@ -119,17 +130,16 @@ void plug_frame_update(PlugState state) {
 
   ClearBackground(BACKGROUND_COLOUR);
 
-  DrawTextEx(state.font, state_as_string(global_state), (Vector2){.x=2, .y=2}, state.fontsize, 0.0, RAYWHITE);
-
-  DrawPoly(center, NUM_OF_ACHROS, outer_radius, 360.0/(20.0*2.0), RAYWHITE);
+  DrawPoly(center, NUM_OF_ACHROS, outer_radius, 360.0/(20.0*2.0), LIGHT_COLOUR);
   DrawPoly(center, NUM_OF_ACHROS, outer_radius - 5*RECT_HEIGHT - 10, 360.0/(20.0*2.0), BACKGROUND_COLOUR);
   DrawPolyLinesEx(center, NUM_OF_ACHROS, outer_radius, 360.0/(20.0*2.0), 4, GRAY);
   DrawPolyLinesEx(center, NUM_OF_ACHROS, outer_radius - 5*RECT_HEIGHT - 10, 360.0/(20.0*2.0), 4, GRAY);
 
-  DrawRectangleRec(ctrl_panel, GRAY);
+  DrawRectangleRec(ctrl_panel, CTRLPANEL_COLOUR);
 
-  DrawRectangleRec(start_stop_btn_rect, DARKGRAY);
-  DrawTextEx(state.font, button_text(ani_state), (Vector2){.x=text_pos.x, .y=text_pos.y}, state.fontsize, 0, BLACK);
+  DrawRectangleRec(pause_btn_rect, BUTTON_COLOUR);
+  DrawRectangleRoundedLines(pause_btn_rect, 0.35, 32, 2, GRAY);
+  DrawTextEx(state.font, button_text(ani_state), (Vector2){.x=text_pos.x, .y=text_pos.y}, state.fontsize, 0, BUTTON_TEXT_COLOUR);
 
   DrawRectanglePro(teamA_rect, (Vector2){0}, rectA_rot, TEAM_A_COLOUR);
   DrawRectanglePro(teamB_rect, (Vector2){0}, rectB_rot, TEAM_B_COLOUR);
