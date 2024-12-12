96b73bb0568d6ee4987418b3a1b79475fdab7882
Stephen Molloy <sdmolloy@gmail.com>
2024-12-12 15:47:56 +0100

    Step button added

--- src/plugin.c
+++ src/plugin.c
@@ -29,12 +29,14 @@
 typedef enum {
   ANI_PAUSED,
   ANI_RUNNING,
+  ANI_STEPPING,
 } Ani_State;
 
-char *button_text(Ani_State state) {
+char *pause_button_text(Ani_State state) {
   switch (state) {
     case ANI_PAUSED: return "Run";
     case ANI_RUNNING: return "Pause";
+    case ANI_STEPPING: return "Run";
   }
 }
 
@@ -72,11 +74,11 @@ void plug_frame_update(PlugState state) {
       break;
     }
     case MOVING: {
-        if (elapsed_time > MOVING_TIME) {
-          elapsed_time = 0.0;
-          global_state = WORKING;
-          frame_number = (frame_number + 1) % state.num_working_days;
-        }
+      if (elapsed_time > MOVING_TIME) {
+        elapsed_time = 0.0;
+        global_state = WORKING;
+        frame_number = (frame_number + 1) % state.num_working_days;
+      }
       if ((ani_state == ANI_RUNNING) || (elapsed_time < MOVING_TIME)) {
         elapsed_time += GetFrameTime();
         size_t next_frame = (frame_number + 1) % state.num_working_days;
@@ -114,15 +116,32 @@ void plug_frame_update(PlugState state) {
     .width=ctrl_panel.width/2-2*PANELPADDING, 
     .height=BTN_HEIGHT
   };
-  Vector2 text_pos = MeasureTextEx(state.font, button_text(ani_state), state.fontsize, 0);
-  text_pos.x = pause_btn_rect.x + pause_btn_rect.width/2 - text_pos.x/2;
-  text_pos.y = pause_btn_rect.y + pause_btn_rect.height/2 - text_pos.y/2;
+  Vector2 pause_text_pos = MeasureTextEx(state.font, pause_button_text(ani_state), state.fontsize, 0);
+  pause_text_pos.x = pause_btn_rect.x + pause_btn_rect.width/2 - pause_text_pos.x/2;
+  pause_text_pos.y = pause_btn_rect.y + pause_btn_rect.height/2 - pause_text_pos.y/2;
+
+  Rectangle step_btn_rect = {
+    .x=ctrl_panel.x+PANELPADDING, 
+    .y=ctrl_panel.y+ctrl_panel.height-PANELPADDING-BTN_HEIGHT,
+    .width=ctrl_panel.width/2-2*PANELPADDING, 
+    .height=BTN_HEIGHT
+  };
+  Vector2 step_text_pos = MeasureTextEx(state.font, "Step", state.fontsize, 0);
+  step_text_pos.x = step_btn_rect.x + step_btn_rect.width/2 - step_text_pos.x/2;
+  step_text_pos.y = step_btn_rect.y + step_btn_rect.height/2 - step_text_pos.y/2;
 
   if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
     Vector2 click_loc = GetMousePosition();
     if (CheckCollisionPointRec(click_loc, pause_btn_rect)) {
       if (ani_state == ANI_RUNNING) ani_state = ANI_PAUSED;
       else if (ani_state == ANI_PAUSED) ani_state = ANI_RUNNING;
+    }
+    if (CheckCollisionPointRec(click_loc, step_btn_rect)) {
+      if (ani_state == ANI_PAUSED) {
+        elapsed_time = 0.0;
+        global_state = MOVING;
+        // frame_number = (frame_number + 1) % state.num_working_days;
+      }
     }
   }
 
@@ -139,7 +158,11 @@ void plug_frame_update(PlugState state) {
 
   DrawRectangleRec(pause_btn_rect, BUTTON_COLOUR);
   DrawRectangleRoundedLines(pause_btn_rect, 0.35, 32, 2, GRAY);
-  DrawTextEx(state.font, button_text(ani_state), (Vector2){.x=text_pos.x, .y=text_pos.y}, state.fontsize, 0, BUTTON_TEXT_COLOUR);
+  DrawTextEx(state.font, pause_button_text(ani_state), pause_text_pos, state.fontsize, 0, BUTTON_TEXT_COLOUR);
+
+  DrawRectangleRec(step_btn_rect, BUTTON_COLOUR);
+  DrawRectangleRoundedLines(step_btn_rect, 0.35, 32, 2, GRAY);
+  DrawTextEx(state.font, "Step", step_text_pos, state.fontsize, 0, BUTTON_TEXT_COLOUR);
 
   DrawRectanglePro(teamA_rect, (Vector2){0}, rectA_rot, TEAM_A_COLOUR);
   DrawRectanglePro(teamB_rect, (Vector2){0}, rectB_rot, TEAM_B_COLOUR);
@@ -148,35 +171,35 @@ void plug_frame_update(PlugState state) {
   DrawRectanglePro(teamE_rect, (Vector2){0}, rectE_rot, TEAM_E_COLOUR);
 
   char *teamA_task = state.job_list_A[frame_number].description.data;
-  text_pos = (Vector2){.x=ctrl_panel.x + 5, .y=ctrl_panel.y + 2};
+  Vector2 desc_text_pos = (Vector2){.x=ctrl_panel.x + 5, .y=ctrl_panel.y + 2};
 
-  DrawTextEx(state.smallfont, "Team A task: ", text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += state.smallfontsize;
-  DrawTextEx(state.smallfont, teamA_task, text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += 2 * state.smallfontsize + PANELPADDING;
+  DrawTextEx(state.smallfont, "Team A task: ", desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += state.smallfontsize;
+  DrawTextEx(state.smallfont, teamA_task, desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += 2 * state.smallfontsize + PANELPADDING;
 
   char *teamB_task = state.job_list_B[frame_number].description.data;
-  DrawTextEx(state.smallfont, "Team B task: ", text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += state.smallfontsize;
-  DrawTextEx(state.smallfont, teamB_task, text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += 2 * state.smallfontsize + PANELPADDING;
+  DrawTextEx(state.smallfont, "Team B task: ", desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += state.smallfontsize;
+  DrawTextEx(state.smallfont, teamB_task, desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += 2 * state.smallfontsize + PANELPADDING;
 
   char *teamC_task = state.job_list_C[frame_number].description.data;
-  DrawTextEx(state.smallfont, "Team C task: ", text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += state.smallfontsize;
-  DrawTextEx(state.smallfont, teamC_task, text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += 2 * state.smallfontsize + PANELPADDING;
+  DrawTextEx(state.smallfont, "Team C task: ", desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += state.smallfontsize;
+  DrawTextEx(state.smallfont, teamC_task, desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += 2 * state.smallfontsize + PANELPADDING;
 
   char *teamD_task = state.job_list_D[frame_number].description.data;
-  DrawTextEx(state.smallfont, "Team D task: ", text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += state.smallfontsize;
-  DrawTextEx(state.smallfont, teamD_task, text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += 2 * state.smallfontsize + PANELPADDING;
+  DrawTextEx(state.smallfont, "Team D task: ", desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += state.smallfontsize;
+  DrawTextEx(state.smallfont, teamD_task, desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += 2 * state.smallfontsize + PANELPADDING;
 
   char *teamE_task = state.job_list_E[frame_number].description.data;
-  DrawTextEx(state.smallfont, "Team E task: ", text_pos, state.smallfontsize, 0, DARK_COLOUR);
-  text_pos.y += state.smallfontsize;
-  DrawTextEx(state.smallfont, teamE_task, text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  DrawTextEx(state.smallfont, "Team E task: ", desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
+  desc_text_pos.y += state.smallfontsize;
+  DrawTextEx(state.smallfont, teamE_task, desc_text_pos, state.smallfontsize, 0, DARK_COLOUR);
 
   EndDrawing();
 }
